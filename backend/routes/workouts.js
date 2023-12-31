d2be10d3fdcf46abb81ed14b6f9a129e67059b14
Walsh Quigley <86450744+Walsh-Quigley@users.noreply.github.com>
2023-12-31 11:26:07 -0800

    model working, tested withpostman

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req,res) => {
-    res.json({mssg: 'POST a new workout'})
+router.post('/', async(req,res) => {
+    const {title, reps, weight} = req.body
+
+    try{
+        const workout = await Workout.create({title, reps, weight})
+        res.status(200).json({workout})
+    }catch (error){
+        res.status(400).json({error})
+    }
 })
 
 // DELETE a workout
